4eaa999d6e2d85c6c98f3ab0dc93ec2eb7b94653
kaua <kauamuniz001@gmail.com>
2025-11-05 04:59:48 -0300

    Criação do arquivo responsável pela remoção de informações

---
+++ 6- Excluir.py
@@ -0,0 +1,15 @@
+import sqlite3 as sq
+
+
+connect = sq.connect("Steam.db")
+cursor = connect.cursor()
+
+cursor.execute(
+    """
+    DELETE FROM jogo
+    WHERE nome = 'FIFA 25'
+    """
+)
+
+connect.commit()
+print("Jogo excluido com sucesso!")
